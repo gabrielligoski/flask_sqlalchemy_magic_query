26a45ff53619a032e2368064f8ea3a106decc714
gabrielligoski <gabriel.ligoski@gmail.com>
2024-02-14 18:56:56 -0300

    feat: upgrading version

--- flask_sqlalchemy_magic_query/__init__.py
+++ flask_sqlalchemy_magic_query/__init__.py
@@ -5,5 +5,5 @@ Converts HTTP URL query string parameters to flask_sqlalchemy query results.
 """
 
 __all__ = ["flask_sqlalchemy_magic_query"]
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = 'Gabriel Ligoski'
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='flask_sqlalchemy_magic_query',
-    version='0.1.0',
+    version='0.1.1',
     description='Converts HTTP URL query string parameters to flask_sqlalchemy query results',
     url='https://github.com/gabrielligoski/flask_sqlalchemy_magic_query',
     author='Gabriel Ligoski',
